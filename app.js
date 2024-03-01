f2066d30d8f8d95ecbab47d18685ad3c5227a8b3
PinYi Lee <pinyi_lee@icloud.com>
2024-03-01 12:05:24 +0800

    製作磚塊

--- app.js
+++ app.js
@@ -7,10 +7,55 @@ let circle_y = 60;
 let radius = 20;
 let xSpeed = 20;
 let ySpeed = 20;
+let ground_x = 100;
+let ground_y = 500;
+let ground_height = 5;
+let brickArray = [];
+
+function getRandomArbitrary(min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+}
+
+class Brick {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 50;
+    this.height = 50;
+    brickArray.push(this);
+  }
+  drawBrick() {
+    ctx.fillStyle = "lightGreen";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+//製作所有的方塊
+for (let i = 0; i < 10; i++) {
+  new Brick(getRandomArbitrary(0, 950), getRandomArbitrary(0, 550));
+}
+
+c.addEventListener("mousemove", (e) => {
+  ground_x = e.clientX;
+});
 
 function drawCircle() {
+  // 確認球是否打到橘色地板
+  if (
+    circle_x >= ground_x - radius &&
+    circle_x <= ground_x + 200 + radius &&
+    circle_y >= ground_y - radius &&
+    circle_y <= ground_y + radius
+  ) {
+    if (ySpeed > 0) {
+      circle_y -= 50;
+      ySpeed *= -1;
+    } else {
+      circle_y += 50;
+    }
+    ySpeed *= -1;
+  }
   //確認球有沒有打到邊界
-
   //確認右邊邊界
   if (circle_x >= canvasWidth - radius) {
     xSpeed *= -1;
@@ -36,6 +81,15 @@ function drawCircle() {
   //畫出黑色背景
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  //畫出所有的方塊
+  brickArray.forEach((brick) => {
+    brick.drawBrick();
+  });
+
+  //畫出可控制的地板
+  ctx.fillStyle = "orange";
+  ctx.fillRect(ground_x, ground_y, 200, ground_height);
 
   //畫出圓球
   //(x,y,radius,startAngle,endAngle)
