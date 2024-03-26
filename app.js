ec470f4ca79a5f63d94cbc0f57b58f93f3ff7840
waiting <waiting@waitingde-MacBook-Pro.local>
2024-03-26 17:59:54 +0800

    add methods for 10sec coundown

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ let ground_y = 500;
 let ground_height = 5;
 let brickArray = [];
 let brickCount = 0;
+let gameOver = false;
+let countdownSec = 10;
+let stopIntervalId;
+
+const start = document.querySelector(".start");
+const countdown = document.querySelector(".countdown");
+countdown.textContent = "";
+start.addEventListener("click", startGame);
 
 //創建磚塊資料與方法
 class Brick {
@@ -71,9 +79,6 @@ class Brick {
     );
   }
 }
-for (let i = 0; i < 10; i++) {
-  new Brick();
-}
 
 //事件監聽器使用滑鼠移動，左右滑動板子
 canvasArea.addEventListener("mousemove", (e) => {
@@ -85,91 +90,140 @@ function defaultBackground() {
   ctx.fillStyle = "wheat";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 }
-
-function drawCircle() {
-  //確認球是否有碰到磚塊，碰到後消失
-  brickArray.forEach((brick) => {
-    if (brick.visible && brick.touchingBall(circle_x, circle_y, radius)) {
-      brickCount++;
-      brick.visible = false;
-      //從磚塊下方撞擊
-      if (circle_y >= brick.y + unit) {
-        ySpeed *= -1;
-      }
-      //從磚塊上方撞擊
-      else if (circle_y <= brick.y) {
-        ySpeed *= -1;
-      }
-      //從磚塊左方撞擊
-      else if (circle_x <= brick.x) {
-        xSpeed *= -1;
-      }
-      //從磚塊右方撞擊
-      else if (circle_x >= brick.x + unit) {
-        xSpeed *= -1;
-      }
-
-      if (brickCount == 10) {
-        alert("Game Over!!");
-        clearInterval(stopIntervalId);
-      }
-    }
-  });
-
-  //確認球是否打到地板，改變y軸的方向，製造出回彈效果
-  if (
-    circle_x >= ground_x - radius &&
-    circle_x <= ground_x + 100 + radius &&
-    circle_y >= ground_y - radius &&
-    circle_y <= ground_y + radius
-  ) {
-    if (ySpeed > 0) {
-      circle_y -= 50;
-      ySpeed *= -1;
-    } else {
-      circle_y += 50;
-    }
-    ySpeed *= -1;
+function defaultGameStates() {
+  gameOver = false;
+  brickCount = 0;
+  countdownSec = 10;
+  circle_x = 160;
+  circle_y = 60;
+  xSpeed = 20;
+  ySpeed = 20;
+  brickArray = [];
+  for (let i = 0; i < 10; i++) {
+    new Brick();
   }
-
-  //確認球有沒有打到邊界：球x座標大於（畫布寬度減去半徑）就是碰到畫布邊界了
-  if (circle_x >= canvasWidth - radius) {
-    xSpeed *= -1;
-  }
-  if (circle_x <= radius) {
-    xSpeed *= -1;
-  }
-  if (circle_y >= canvasHeight - radius) {
-    ySpeed *= -1;
-  }
-  if (circle_y <= radius) {
-    ySpeed *= -1;
-  }
-
-  //製造出球“持續移動”
-  circle_x += xSpeed;
-  circle_y += ySpeed;
-
-  //繪製背景
-  defaultBackground();
-
-  //畫出所有的方塊，在背景生成後，接續生成磚塊
-  brickArray.forEach((brick) => {
-    if (brick.visible) {
-      brick.drawBrick();
-    }
-  });
-
-  //畫出板子
-  ctx.fillStyle = "white";
-  ctx.fillRect(ground_x, ground_y, 100, ground_height);
-
-  //畫出圓球(x,y,radius,startAngle,endAngle)
-  ctx.beginPath(); //開始新路徑
-  ctx.arc(circle_x, circle_y, radius, 0, 2 * Math.PI); //畫出圓球
-  ctx.stroke(); //繪製相連的點形成 路徑
-  ctx.fillStyle = "lightgrey";
-  ctx.fill(); //填充該路徑
 }
 
-let stopIntervalId = setInterval(drawCircle, 25);
+function startGame() {
+  clearInterval(stopIntervalId);
+  defaultGameStates();
+  drawBallAndBrick();
+  startCountdown();
+}
+
+//生成球與磚塊並碰撞
+function drawBallAndBrick() {
+  start.disabled = true;
+
+  function drawCircle() {
+    //確認球是否有碰到磚塊，碰到後消失
+    brickArray.forEach((brick) => {
+      if (brick.visible && brick.touchingBall(circle_x, circle_y, radius)) {
+        brickCount++;
+        brick.visible = false;
+        //從磚塊下方撞擊
+        if (circle_y >= brick.y + unit) {
+          ySpeed *= -1;
+        }
+        //從磚塊上方撞擊
+        else if (circle_y <= brick.y) {
+          ySpeed *= -1;
+        }
+        //從磚塊左方撞擊
+        else if (circle_x <= brick.x) {
+          xSpeed *= -1;
+        }
+        //從磚塊右方撞擊
+        else if (circle_x >= brick.x + unit) {
+          xSpeed *= -1;
+        }
+      }
+    });
+
+    //確認球是否打到地板，改變y軸的方向，製造出回彈效果
+    if (
+      circle_x >= ground_x - radius &&
+      circle_x <= ground_x + 100 + radius &&
+      circle_y >= ground_y - radius &&
+      circle_y <= ground_y + radius
+    ) {
+      if (ySpeed > 0) {
+        circle_y -= 50;
+        ySpeed *= -1;
+      } else {
+        circle_y += 50;
+      }
+      ySpeed *= -1;
+    }
+
+    //確認球有沒有打到邊界：球x座標大於（畫布寬度減去半徑）就是碰到畫布邊界了
+    if (circle_x >= canvasWidth - radius) {
+      xSpeed *= -1;
+    }
+    if (circle_x <= radius) {
+      xSpeed *= -1;
+    }
+    if (circle_y >= canvasHeight - radius) {
+      ySpeed *= -1;
+    }
+    if (circle_y <= radius) {
+      ySpeed *= -1;
+    }
+
+    //製造出球“持續移動”
+    circle_x += xSpeed;
+    circle_y += ySpeed;
+
+    //繪製背景
+    defaultBackground();
+
+    //畫出所有的方塊，在背景生成後，接續生成磚塊
+    brickArray.forEach((brick) => {
+      if (brick.visible) {
+        brick.drawBrick();
+      }
+    });
+
+    //畫出板子
+    if (!gameOver) {
+      ctx.fillStyle = "white";
+      ctx.fillRect(ground_x, ground_y, 100, ground_height);
+
+      //畫出圓球(x,y,radius,startAngle,endAngle)
+      ctx.beginPath(); //開始新路徑
+      ctx.arc(circle_x, circle_y, radius, 0, 2 * Math.PI); //畫出圓球
+      ctx.stroke(); //繪製相連的點形成 路徑
+      ctx.fillStyle = "lightgrey";
+      ctx.fill(); //填充該路徑
+    }
+  }
+  stopIntervalId = setInterval(drawCircle, 25);
+}
+
+function displayCountdown() {
+  countdown.textContent = `剩餘時間：${countdownSec}秒`;
+}
+function startCountdown() {
+  displayCountdown();
+  let countdownPlayer = setInterval(() => {
+    countdownSec -= 1;
+    displayCountdown();
+    start.disabled = true;
+    if (countdownSec <= 0 || (countdownSec > 0 && brickCount == 10)) {
+      clearInterval(countdownPlayer);
+      checkGameOver();
+    }
+  }, 1000);
+}
+
+function checkGameOver() {
+  clearInterval(stopIntervalId);
+  if (brickCount == 10) {
+    alert("挑戰成功");
+  } else {
+    alert("挑戰失敗");
+  }
+  start.disabled = false;
+  gameOver = true;
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+}
